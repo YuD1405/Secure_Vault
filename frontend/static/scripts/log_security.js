0aa208162b2beef40d0231c5a05f007ec9960f43
Quang Duy Phạm <quangduypham1405@gmail.com>
2025-06-19 11:15:06 +0700

    QDuy _ 19/6/2025 _ 11:15 AM _ Update: Logger full function with basic UI

---
+++ frontend/static/scripts/log_security.js
@@ -0,0 +1,17 @@
+document.getElementById("refreshBtn").addEventListener("click", async () => {
+  try {
+    const res = await fetch("/utils/log_security");
+    const html = await res.text();
+
+    // Lấy phần bảng mới ra khỏi response
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    const newTable = doc.querySelector("#logContainer");
+
+    // Thay thế bảng cũ
+    document.getElementById("logContainer").innerHTML = newTable.innerHTML;
+  } catch (err) {
+    alert("Lỗi khi tải lại bảng log!");
+    console.error(err);
+  }
+});
